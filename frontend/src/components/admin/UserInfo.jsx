111ef71d47075d35c636be16d768a6147003988b
Priyanshi <126345468+priyanshi480@users.noreply.github.com>
2025-05-06 14:16:28 +0530

    Add files via upload

---
+++ frontend/src/components/admin/UserInfo.jsx
@@ -0,0 +1,165 @@
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
+import Table from 'react-bootstrap/Table';
+import Alert from 'react-bootstrap/Alert';
+import { Container } from 'react-bootstrap';
+import Collapse from 'react-bootstrap/Collapse';
+import Form from 'react-bootstrap/Form';
+import Footer from '../common/FooterC'
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import { toast } from 'react-hot-toast';
+
+import axios from 'axios';
+
+const UserInfo = () => {
+   const navigate = useNavigate();
+   const [ordinaryList, setOrdinaryList] = useState([]);
+   const [toggle, setToggle] = useState({})
+
+   // const [count, setCount] = useState(0)
+
+   const [updateUser, setUpdateUser] = useState({
+      name: '',
+      email: '',
+      phone: '',
+   })
+
+   const handleChange = (e) => {
+      setUpdateUser({ ...updateUser, [e.target.name]: e.target.value })
+   }
+
+   const handleSubmit = async (user_id) => {
+      if (updateUser === "") {
+         alert("atleast 1 fields need to be fill")
+      }
+      else {
+         window.confirm("Are you sure you want to Update the user?");
+         axios.put(`http://localhost:8000/user/${user_id}`, updateUser)
+            .then((res) => {
+               alert(`user updated successfully`)
+               JSON.stringify(res.data)
+            })
+            .catch((err) => {
+               console.log(err)
+            })
+      }
+   }
+
+   useEffect(() => {
+      const getOrdinaryRecords = async () => {
+         try {
+            const response = await axios.get('http://localhost:8000/OrdinaryUsers');
+            const ordinary = response.data;
+            setOrdinaryList(ordinary)
+         } catch (error) {
+            console.log(error);
+         }
+      };
+      getOrdinaryRecords();
+   }, [navigate]);
+
+   const deleteUser = async (userId) => {
+      confirmAlert({
+         title: 'Confirm Deletion',
+         message: 'Are you sure you want to delete this user?',
+         buttons: [
+            {
+               label: 'Yes',
+               onClick: async () => {
+                  try {
+                     await axios.delete(`http://localhost:8000OrdinaryUsers/${userId}`);
+                     setOrdinaryList(ordinaryList.filter((user) => user._id !== userId));
+                     toast.success('User deleted successfully');
+                  } catch (error) {
+                     toast.error('Failed to delete user');
+                     console.error(error);
+                  }
+               }
+            },
+            {
+               label: 'No',
+               onClick: () => null
+            }
+         ]
+      });
+   }
+
+   const handleToggle = (complaintId) => {
+      setToggle((prevState) => ({
+         ...prevState,
+         [complaintId]: !prevState[complaintId],
+      }));
+   };
+
+   return (
+      <>
+         <div className="body">
+
+            <Container>
+               <Table striped bordered hover>
+                  <thead>
+                     <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Action</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     {ordinaryList.length > 0 ? (
+                        ordinaryList.map((user) => {
+                           const open = toggle[user._id] || false;
+
+                           return (
+                              <tr key={user._id}>
+                                 <td>{user.name}</td>
+                                 <td>{user.email}</td>
+                                 <td>{user.phone}</td>
+                                 <td><Button onClick={() => handleToggle(user._id)}
+                                    aria-controls={`collapse-${user._id}`}
+                                    aria-expanded={open}
+                                    className='mx-2'
+                                    variant="outline-warning">
+                                    Update
+                                 </Button>
+                                    <Collapse in={open}>
+                                       <Form onSubmit={() => handleSubmit(user._id)} className='p-5'>
+                                          <Form.Group className="mb-3" controlId="formBasic">
+                                             <Form.Label>Full Name </Form.Label>
+                                             <Form.Control name='name' value={updateUser.name} onChange={handleChange} type="text" placeholder="Enter name" />
+                                          </Form.Group>
+                                          <Form.Group className="mb-3" controlId="formBasicEmail">
+                                             <Form.Label>Email address</Form.Label>
+                                             <Form.Control name='email' value={updateUser.email} onChange={handleChange} type="email" placeholder="Enter email" />
+                                          </Form.Group>
+
+                                          <Form.Group className="mb-3" controlId="formBasicTel">
+                                             <Form.Label>Phone</Form.Label>
+                                             <Form.Control name='phone' value={updateUser.phone} onChange={handleChange} type="tel" placeholder="Enter Phone no." />
+                                          </Form.Group>
+
+                                          <Button size='sm' variant="outline-success" type="submit">
+                                             Submit
+                                          </Button>
+                                       </Form>
+                                    </Collapse>
+                                    <Button onClick={() => deleteUser(user._id)} className='mx-2' variant="outline-danger">Delete</Button></td>
+                              </tr>
+                           )
+                        })
+                     ) : (
+                        <Alert variant="info">
+                           <Alert.Heading>No Users to show</Alert.Heading>
+                        </Alert>
+                     )}
+                  </tbody>
+               </Table>
+            </Container>
+         </div>
+            <Footer />
+      </>
+   )
+}
+export default UserInfo
